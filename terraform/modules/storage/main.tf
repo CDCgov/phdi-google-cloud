05011ef2623b9d43d65dc551b595a440bb576098
DanSkylight <dan@skylight.digital>
2022-07-27 21:48:31 -0400

    Add a toy storage bucket for the toy pipeline we are building to get up to speed with GCP.

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -3,12 +3,26 @@ resource "random_id" "instance_id" {
   byte_length = 8
 }
 
+resource "random_id" "toy_bucket_id" {
+  byte_length = 8
+}
+
 resource "random_id" "function_bucket_id" {
   byte_length = 8
 }
 
 resource "google_storage_bucket" "tfstate" {
   name          = "bucket-tfstate-${random_id.instance_id.hex}"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket" "toybucket" {
+  name          = "bucket-toybucket-${random_id.toy_bucket_id.hex}"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
