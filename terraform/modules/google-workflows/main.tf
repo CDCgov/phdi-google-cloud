07160eb041259233bd334370f718e03d893d0e9e
DanSkylight <dan@skylight.digital>
2022-08-01 17:36:17 -0400

    Fix indentation in google-workflows/main.

--- terraform/modules/google-workflows/main.tf
+++ terraform/modules/google-workflows/main.tf
@@ -7,18 +7,18 @@ resource "google_workflows_workflow" "workflow-1" {
 }
 
 resource "google_eventarc_trigger" "toy-bucket-new-file" {
-    name = "toy-bucket-new-file"
-    location = "us"
-    matching_criteria {
-        attribute = "type"
-        value = "google.cloud.storage.object.v1.finalized"
-    }
-    matching_criteria {
-        attribute = "bucket"
-        value     = var.toybucket
-    }
-    destination {
-        workflow = google_workflows_workflow.workflow-1.id
-    }
-    service_account= var.workflow_service_account
+  name = "toy-bucket-new-file"
+  location = "us"
+  matching_criteria {
+      attribute = "type"
+      value = "google.cloud.storage.object.v1.finalized"
+  }
+  matching_criteria {
+      attribute = "bucket"
+      value     = var.toybucket
+  }
+  destination {
+      workflow = google_workflows_workflow.workflow-1.id
+  }
+  service_account= var.workflow_service_account
 }
