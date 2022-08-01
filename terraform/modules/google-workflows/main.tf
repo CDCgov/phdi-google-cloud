435bff2510e954c8430a5cf14d1d9dd751f1baf5
DanSkylight <dan@skylight.digital>
2022-08-01 17:19:08 -0400

    Correct region for workflow and trigger.

--- terraform/modules/google-workflows/main.tf
+++ terraform/modules/google-workflows/main.tf
@@ -5,7 +5,7 @@
 
 resource "google_workflows_workflow" "workflow-1" {
   name          = "workflow-1"
-  region        = "us-central1"
+  region        = "us-west1"
   description   = "Magic"
   service_account = var.workflow_service_account
   source_contents = file("../../../google-workflows/workflow-1.yaml")
@@ -13,7 +13,7 @@ resource "google_workflows_workflow" "workflow-1" {
 
 resource "google_eventarc_trigger" "toy-bucket-new-file" {
     name = "toy-bucket-new-file"
-    location = "us-west1"
+    location = "us"
     matching_criteria {
         attribute = "type"
         value = "google.cloud.storage.object.v1.finalized"
