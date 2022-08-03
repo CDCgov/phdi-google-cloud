e22ecd2ce8ca56df809da9ce11d7a2b9a27ffd09
Brady Fausett at Skylight <brady@skylight.digital>
2022-08-03 15:54:12 -0600

    Fixed the formatting issues with the variables file

--- terraform/modules/fhir/variables.tf
+++ terraform/modules/fhir/variables.tf
@@ -1,29 +1,29 @@
 variable "region" {
-  type = string
+  type        = string
   description = "The region of the project and the related data sets"
-  default = "us-west1"
+  default     = "us-west1"
 }
 
 variable "time_zone" {
-  type = string
+  type        = string
   description = "The default timezone used by this dataset. Must be a either a valid IANA time zone name or empty, which defaults to UTC."
-  default = "UTC"
+  default     = "UTC"
 }
 
 variable "dataset_name" {
-  type = string
+  type        = string
   description = "The resource name for the Dataset."
-  default = "PHDI_DATASET"
+  default     = "PHDI_DATASET"
 }
 
 variable "fhirstore_name" {
-  type = string
+  type        = string
   description = "The resource name for the FhirStore."
-  default = "PHDI_FHIRSTORE"
+  default     = "PHDI_FHIRSTORE"
 }
 
 variable "fhir_version" {
-  type = string
+  type        = string
   description = "The FHIR specification version. Default value is STU3. Possible values are DSTU2, STU3, and R4."
-  default = "R4"
+  default     = "R4"
 }
