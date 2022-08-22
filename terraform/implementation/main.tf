f064a933e2b052a25fd488c55ddb8feb79c6fdc0
Nick Clyde <nick@clyde.tech>
2022-08-22 15:46:02 -0700

    Fix project ID

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 resource "google_project_service" "enable_google_apis" {
   count   = length(var.gcp_services_list)
-  project = google_project.this.project_id
+  project = var.project_id
   service = var.gcp_services_list[count.index]
 
   disable_dependent_services = true
