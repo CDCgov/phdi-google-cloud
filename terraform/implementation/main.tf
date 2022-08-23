27a1468a3c2b5be992a854d9bfd24e4c63af6657
Nick Clyde <nick@clyde.tech>
2022-08-23 09:34:57 -0700

    Add Terraform configuration to enable GCP APIs (#18)
    * Add Terraform configuration to enable GCP APIs
    
    * Fix project ID
    
    * Remove source.googleapis.com as it is not supported

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -4,6 +4,14 @@ provider "google" {
   zone    = var.zone
 }
 
+resource "google_project_service" "enable_google_apis" {
+  count   = length(var.gcp_services_list)
+  project = var.project_id
+  service = var.gcp_services_list[count.index]
+
+  disable_dependent_services = true
+}
+
 module "storage" {
   source     = "../modules/storage"
   project_id = var.project_id
--- terraform/implementation/variables.tf
+++ terraform/implementation/variables.tf
@@ -11,3 +11,39 @@ variable "zone" {
   description = "value of the GCP zone to deploy to"
   default     = "us-east1-b"
 }
+
+variable "gcp_services_list" {
+  description = "The list of GCP APIs necessary for the project."
+  type        = list(string)
+  default = [
+    "artifactregistry.googleapis.com",
+    "bigquery.googleapis.com",
+    "bigquerymigration.googleapis.com",
+    "bigquerystorage.googleapis.com",
+    "cloudapis.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "clouddebugger.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudtrace.googleapis.com",
+    "compute.googleapis.com",
+    "containerregistry.googleapis.com",
+    "datastore.googleapis.com",
+    "dns.googleapis.com",
+    "eventarc.googleapis.com",
+    "healthcare.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "oslogin.googleapis.com",
+    "pubsub.googleapis.com",
+    "run.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sql-component.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "storage.googleapis.com",
+    "workflowexecutions.googleapis.com",
+    "workflows.googleapis.com"
+  ]
+}
