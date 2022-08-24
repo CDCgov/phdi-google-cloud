13b81e81b3860694a540e2cec893247c38233f75
Brady Fausett at Skylight <brady@skylight.digital>
2022-08-24 15:28:31 -0600

    Initial Commit with code and tests
    Added a new file to the gitignore.

---
+++ cloud-functions/http_standardize_names/main.py
@@ -0,0 +1,42 @@
+from http.client import BAD_REQUEST
+import json
+import logging
+import functions_framework
+import flask
+from phdi.fhir.harmonization.standardization import standardize_names
+
+
+@functions_framework.http
+def http_standardize_names(request: flask.Request) -> flask.Response:
+    """Given either a FHIR bundle or a FHIR resource, transform all names
+    contained in any resource in the input.  The default standardization
+    behavior is our defined non-numeric, space-trimming, full
+    capitalization standardization, but other modes may be specified.
+
+    :param data: Either a FHIR bundle or a FHIR-formatted JSON dict
+    :return: The bundle or resource with names appropriately standardized
+    """
+    # Validate request header.
+    if request.headers.get("Content-Type") != "application/json":
+        logging.error("Header must inclue: 'Content-Type:application/json'.")
+        return {
+            "status": 400,
+            "summary": "Bad request",
+            "description": "Header must inclue: 'Content-Type:application/json'.",
+        }
+
+    try:
+        request_json = request.get_json(silent=False)
+    except BaseException as error:
+        logging.error(error)
+
+        return {
+            "status": 400,
+            "summary": "Invalid request body",
+            "description": "Invalid JSON",
+        }
+
+    # Perform the name standardization
+    response = standardize_names(request_json)
+
+    return response
---
+++ cloud-functions/http_standardize_names/test_http_standardize_name.py
@@ -0,0 +1,33 @@
+from main import http_standardize_names
+import pytest
+from unittest import mock
+
+
+def test_standardize_names_bad_header():
+    request = mock.Mock(headers={"Content-Type": "not-application/json"})
+    assert http_standardize_names(request) == {
+        "status": 400,
+        "summary": "Bad request",
+        "description": "Header must inclue: 'Content-Type:application/json'.",
+    }
+
+
+def test_upload_fhir_bundle_bad_body():
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+
+    assert http_standardize_names(request) == {
+        "description": "Invalid JSON",
+        "status": 400,
+        "summary": "Invalid request body",
+    }
+
+
+@mock.patch("main.http_standardize_names")
+def test_upload_fhir_bundle_good_request(patched_standardized_names):
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+    request.get_json.return_value = {
+        {"resourceType": "Bundle"},
+    }
+    http_standardize_names(request)
+
+    patched_standardized_names.assert_called_with(request.get_json())
