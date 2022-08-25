8678c067583dbbea015a0a7c400239bd688eac70
Nick Clyde <nick@clyde.tech>
2022-08-25 10:36:07 -0700

    Build docker image in workflow after creating registry

--- terraform/modules/cloud-run/main.tf
+++ terraform/modules/cloud-run/main.tf
@@ -1,27 +1,11 @@
-resource "null_resource" "docker_build" {
-  triggers = {
-    git_sha = var.git_sha
-  }
-
-  provisioner "local-exec" {
-    working_dir = "../../cloud-run/fhir-converter"
-    command     = <<-EOT
-      gcloud auth configure-docker ${var.region}-docker.pkg.dev
-      docker build -t ${var.region}-docker.pkg.dev/${var.project_id}/phdi-${terraform.workspace}-repository/fhir-converter:${var.git_sha} .
-      docker push ${var.region}-docker.pkg.dev/${var.project_id}/phdi-${terraform.workspace}-repository/fhir-converter:${var.git_sha}
-    EOT
-  }
-}
-
 resource "google_cloud_run_service" "fhir_converter" {
-  depends_on = [null_resource.docker_build]
-  name       = "phdi-${terraform.workspace}-fhir-converter-service"
-  location   = var.region
+  name     = "phdi-${terraform.workspace}-fhir-converter-service"
+  location = var.region
 
   template {
     spec {
       containers {
-        image = "${var.region}.pkg.dev/${var.project_id}/phdi-${terraform.workspace}-repository/fhir-converter:${var.git_sha}"
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/phdi-${terraform.workspace}-repository/fhir-converter:${var.git_sha}"
 
         ports {
           container_port = 8080
