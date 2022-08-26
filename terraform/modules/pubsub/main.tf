236e6e807a2271d0a0b9bbf91059dee9fac3c0e7
DanSkylight <dan@skylight.digital>
2022-08-26 00:10:16 -0400

    Remove project ID from ingestion topic name.

--- terraform/modules/pubsub/main.tf
+++ terraform/modules/pubsub/main.tf
@@ -1,3 +1,3 @@
 resource "google_pubsub_topic" "ingestion_topic" {
-  name = "phdi-${terraform.workspace}-ingestion-topic-${var.project_id}"
+  name = "phdi-${terraform.workspace}-ingestion-topic"
 }
