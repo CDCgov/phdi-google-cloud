29abd6a07b157e6fe9ddce45b0155362a9348e08
Brady Fausett at Skylight <brady@skylight.digital>
2022-08-29 11:57:43 -0600

    Cleaned up code to return proper flask.Repsonse
    Documentation added to each of the functions as well.

--- cloud-functions/utils/__init__.py
+++ cloud-functions/utils/__init__.py
@@ -1,57 +1,82 @@
 import logging
 import flask
 
-""" A Class to hold commonly used functions
-that can be reused by the different cloud functions
 
-TODO: We may want to move this into the SDK
-"""
+def _success() -> flask.Response:
+    result = flask.Response(status="OK", response="Validation Succeeded!")
+    result.status_code = 200
+    return result
+
+
+def _fail(message: str, status: str) -> flask.Response:
+    result = flask.Response(status=status, response=message)
+    result.status_code = 400
+    return result
 
 
 # Validate request header.
 def validate_request_header(
     request: flask.Request, content_type: str
 ) -> flask.Response:
+    """
+    Validate that the request header is of the correct type specified
+    as one of the parameters.
+
+    :param request: flask.Request that shoud contain the header being evaluated
+    :param content_type: The specified content type expected to be in the header
+    :return: A flask.Response object containing the error if the header is missing
+        or will return a generic 200 flask.Response
+    """
     if request.headers.get("Content-Type") != content_type:
-        logging.error("Header must inclue: 'Content-Type:{}'.".format(content_type))
-        return {
-            "status": 400,
-            "summary": "Bad request",
-            "description": "Header must inclue: 'Content-Type:{}'.".format(
-                content_type
-            ),
-        }
-    return request
+        logging.error("Header must include: 'Content-Type:{}'.".format(content_type))
+        return _fail(
+            "Header must include: 'Content-Type:{}'.".format(content_type),
+            "Bad Request",
+        )
+
+    return _success()
 
 
 def validate_request_body_json(request: flask.Request) -> flask.Response:
-    try:
-        request_json = request.get_json(silent=False)
-        return request_json
-    except AttributeError as error:
-        logging.error(error)
-        return {
-            "status": 400,
-            "summary": "Invalid request body",
-            "description": "Invalid JSON",
-        }
+    """
+    Validate that the request body is proper JSON
+
+    :param request: flask.Request that shoud contain the JSON in the body
+    :return: A flask.Response object containing the error if the body is not
+        proper JSON or will return a generic 200 flask.Response
+    """
+
+    if request.is_json():
+        return _success
+    else:
+        return _fail(
+            message="Invalid request body - Invalid JSON", status="Bad Request"
+        )
 
 
 def validate_fhir_bundle_or_resource(request: flask.Request) -> flask.Response:
-    # first ensure that the response body is in proper JSON format
-    request_json = validate_request_body_json(request)
-    if request_json.get("status") is None or request_json.get("status") != 400:
+    """
+    Validate that the request body is proper JSON with either a
+    FHIR Bundle or other FHIR resource
 
-        # Check that the request body contains a FHIR bundle or resource.
-        if request_json.get("resourceType") is None:
-            error_message = (
-                "FHIR Resource Type not specified. "
-                + "The request body must contain a valid FHIR bundle or resource."
-            )
-            logging.error(error_message)
-            return {
-                "status": 400,
-                "summary": "Bad request",
-                "description": error_message,
-            }
-    return request_json
+    :param request: flask.Request that shoud contain the FHIR Bundle or resource
+    as a JSON response in the body
+    :return: A flask.Response object containing the error if the body is not
+        proper JSON or a Bundle or resource
+        otherwise will return a generic 200 flask.Response
+    """
+
+    request_json = request.get_json
+    print("BLAH:")
+    print(request_json)
+
+    # Check that the request body contains a FHIR bundle or resource.
+    if request_json.get("resourceType") is None:
+        error_message = (
+            "FHIR Resource Type not specified. "
+            + "The request body must contain a valid FHIR bundle or resource."
+        )
+        logging.error(error_message)
+        return _fail(message=error_message, status="Bad Request")
+
+    return _success()
--- cloud-functions/utils/test_utils.py
+++ cloud-functions/utils/test_utils.py
@@ -1,9 +1,12 @@
 import copy
+import flask
 import json
 from utils import (
     validate_fhir_bundle_or_resource,
     validate_request_body_json,
     validate_request_header,
+    _fail,
+    _success,
 )
 from unittest import mock
 
@@ -13,47 +16,53 @@ test_request_body = json.load(open("../assets/single_patient_bundle.json", "r"))
 
 def test_utils_bad_header():
     request = mock.Mock(headers={"Content-Type": "not-application/json"})
+    expected_result = _fail(
+        status="Bad Request",
+        message="Header must include: 'Content-Type:application/json'.",
+    )
+    expected_result.status_code = 400
 
     result = validate_request_header(request, "application/json")
-    assert result == {
-        "status": 400,
-        "summary": "Bad request",
-        "description": "Header must inclue: 'Content-Type:application/json'.",
-    }
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
 
 
-def test_utils_bad_body():
+def test_utils_good_header():
     request = mock.Mock(headers={"Content-Type": "application/json"})
-    request.get_json.return_value = ""
-    expected_result = {
-        "status": 400,
-        "summary": "Bad request",
-        "description": "Header must inclue: 'Content-Type:application/json'.",
-    }
+    expected_result = _success()
 
+    result = validate_request_header(request, "application/json")
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
+
+
+def test_utils_good_body():
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+    request.get_json.return_value = test_request_body
+    expected_result = _success
     result = validate_request_body_json(request)
-    print("Blah")
-
-    print(result)
     assert result == expected_result
 
 
-def test_utils_resource_type():
+def test_utils_bad_resource_type():
     request = mock.Mock(headers={"Content-Type": "application/json"})
     body_with_wrong_resource_type = copy.deepcopy(test_request_body)
     body_with_wrong_resource_type["resourceType"] = None
+    print("WELL:")
+    print(body_with_wrong_resource_type)
     error_message = (
         "FHIR Resource Type not specified. "
         + "The request body must contain a valid FHIR bundle or resource."
     )
     request.get_json.return_value = body_with_wrong_resource_type
-    expected_result = {
-        "status": 400,
-        "summary": "Bad request",
-        "description": error_message,
-    }
+    expected_result = _fail(message=error_message, status="Bad Request")
+    expected_result.status_code = 400
     result = validate_fhir_bundle_or_resource(request=request)
-    assert result == expected_result
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
 
 
 def test_utils_request():
