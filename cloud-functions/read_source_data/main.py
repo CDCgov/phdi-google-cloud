207896b1eb04a6b796994f05de6c84dd86d27b5f
DanSkylight <dan@skylight.digital>
2022-08-29 16:30:59 -0400

    Handle publishing failures with logging and writting messages back to storage if they can't be published.

--- cloud-functions/read_source_data/main.py
+++ cloud-functions/read_source_data/main.py
@@ -4,10 +4,12 @@ from phdi.conversion import convert_batch_messages_to_list
 import os
 from google.cloud import pubsub_v1
 from google.cloud import storage
+import json
+
 
 @functions_framework.cloud_event
 def read_source_data(cloud_event):
-    
+
     # Extract buck and file names.
     try:
         filename = cloud_event.data["name"]
@@ -15,7 +17,19 @@ def read_source_data(cloud_event):
     except AttributeError:
         logging.error("Bad CloudEvent payload - 'data' attribute missing.")
     except KeyError:
-        logging.error("Bad CloudEvent payload - 'name' or 'bucket' name was not included.")
+        logging.error(
+            "Bad CloudEvent payload - 'name' or 'bucket' name was not included."
+        )
+        return
+
+    # Load environment variables.
+    try:
+        project_id = os.environ["PROJECT_ID"]
+        ingestion_topic = os.environ["INGESTION_TOPIC"]
+    except KeyError:
+        logging.error(
+            "Missing required environment variables. Values for PROJECT_ID and TOPIC_ID must be set."
+        )
         return
 
     # Determine data type and root template.
@@ -24,11 +38,11 @@ def read_source_data(cloud_event):
         if filename_parts[1] == "elr":
             message_type = "hl7v2"
             root_template = "ORU_R01"
-        
+
         elif filename_parts[1] == "vxu":
             message_type = "hl7v2"
             root_template = "VXU_V04"
-        
+
         elif filename_parts[1] == "ecr":
             message_type = "ccda"
             root_template = "CCD"
@@ -36,7 +50,7 @@ def read_source_data(cloud_event):
         else:
             logging.error("Unknown message type. Messages should be ELR, VXU, or eCR.")
             return
-    
+
     # Read file.
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
@@ -50,18 +64,52 @@ def read_source_data(cloud_event):
         messages = [file_contents]
 
     # Publish messages to pub/sub topic
-    try:  
-        project_id = os.environ["PROJECT_ID"]
-        ingestion_topic = os.environ["INGESTION_TOPIC"]
-    except KeyError: 
-        logging.error("Missing required environment variables. Values for PROJECT_ID and TOPIC_ID must be set.")
-        return
-
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, ingestion_topic)
-    for message in messages:
-        message = message.encode("utf-8")
+    for idx, message in enumerate(messages):
+        pubsub_message = {
+            "message": message,
+            "message_type": message_type,
+            "root_template": root_template,
+        }
+        pubsub_message = json.dumps(pubsub_message).encode("utf-8")
         future = publisher.publish(
-        topic_path, message, origin="read_source_data", message_type=message_type, root_template=root_template
-    )
-    print(future.result())
+            topic_path, pubsub_message, origin="read_source_data"
+        )
+        try:
+            message_id = future.result()
+            logging.info(
+                f"Message {idx} in {filename} was published to {topic_path} with message ID {message_id}."
+            )
+        except Exception as error:
+            error_message = str(error)
+            logging.warning(
+                f"First attempt to publish message {idx} in {filename} failed because {error_message}. Trying again..."
+            )
+            # Retry publishing.
+            try:
+                future = publisher.publish(
+                    topic_path, pubsub_message, origin="read_source_data"
+                )
+                message_id = future.result()
+                logging.info(
+                    f"Message {idx} in {filename} was published to {topic_path} with message ID {message_id}."
+                )
+            # On second failure write the message to storage and continue.
+            except Exception as error:
+                error_message = str(error)
+                logging.error(
+                    f"Publishing message {idx} in {filename} failed because {error_message}."
+                )
+                failure_filename = filename.split("/")
+                failure_filename[0] = "publishing-failures"
+                failure_filename[-1] = (
+                    failure_filename[-1].split(".")[0:-1] + "-{idx}.txt"
+                )
+                failure_filename = "/".join(failure_filename)
+                blob = bucket.blob(failure_filename)
+                blob.upload_from_string(message)
+                logging.info(
+                    "Message {idx} in {filename} was written to {failure_filename} in {bucket_name}."
+                )
+        logging.info(f"Processed {filename}. Any messages that could not be published to the specified pubsub topic were written to {bucket}/publishing-failures/.")
