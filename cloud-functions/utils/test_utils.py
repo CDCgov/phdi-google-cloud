e11c0f24e289cf61f8afcd26558ff140791fd2ce
Brady Fausett at Skylight <109990072+BradySkylight@users.noreply.github.com>
2022-08-29 15:38:41 -0600

    Create the utils module and a test to verify all the functions (#27)
    * Create the utils module and a test to verify all the functions
    
    * Remove print test statements
    
    * add requirements.txt file for utils directory
    
    * fixed test case failing
    
    * Cleaned up code to return proper flask.Repsonse
    
    Documentation added to each of the functions as well.
    
    * Fixed mocks and tests to all pass
    
    * remove unnecessary requirement
    
    * Fix requirements and removed comments

---
+++ cloud-functions/utils/test_utils.py
@@ -0,0 +1,89 @@
+import copy
+import json
+from utils import (
+    validate_fhir_bundle_or_resource,
+    validate_request_body_json,
+    validate_request_header,
+    _fail,
+    _success,
+)
+from unittest import mock
+
+
+test_request_body = json.load(open("../assets/single_patient_bundle.json", "r"))
+
+
+def test_utils_bad_header():
+    request = mock.Mock(headers={"Content-Type": "not-application/json"})
+    expected_result = _fail(
+        status="Bad Request",
+        message="Header must include: 'Content-Type:application/json'.",
+    )
+    expected_result.status_code = 400
+
+    result = validate_request_header(request, "application/json")
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
+
+
+def test_utils_good_header():
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+    expected_result = _success()
+
+    result = validate_request_header(request, "application/json")
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
+
+
+def test_utils_good_body():
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+    request.get_json.return_value = test_request_body
+    expected_result = _success()
+    result = validate_request_body_json(request)
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
+
+
+def test_utils_bad_body():
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+    request.is_json.return_value = False
+    expected_result = _fail(
+        message="Invalid request body - Invalid JSON", status="Bad Request"
+    )
+    result = validate_request_body_json(request)
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
+
+
+def test_utils_bad_resource_type():
+    mock_request = mock.Mock(headers={"Content-Type": "application/json"})
+    body_with_wrong_resource_type = copy.deepcopy(test_request_body)
+    body_with_wrong_resource_type["resourceType"] = None
+
+    error_message = (
+        "FHIR Resource Type not specified. "
+        + "The request body must contain a valid FHIR bundle or resource."
+    )
+    mock_request.get_json.return_value = body_with_wrong_resource_type
+    expected_result = _fail(message=error_message, status="Bad Request")
+    expected_result.status_code = 400
+    result = validate_fhir_bundle_or_resource(request=mock_request)
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
+
+
+def test_utils_request():
+    request = mock.Mock(headers={"Content-Type": "application/json"})
+
+    expected_result = _success()
+    request.get_json.return_value = test_request_body
+    result = validate_fhir_bundle_or_resource(request)
+
+    assert result.status == expected_result.status
+    assert result.status_code == expected_result.status_code
+    assert result.response == expected_result.response
