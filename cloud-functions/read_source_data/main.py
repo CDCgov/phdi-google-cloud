f958b5cfbff6d8779c3843bb2d6407b0953c582f
DanSkylight <dan@skylight.digital>
2022-08-29 23:55:01 -0400

    Improved logging and refactoring.

--- cloud-functions/read_source_data/main.py
+++ cloud-functions/read_source_data/main.py
@@ -5,36 +5,73 @@ import os
 from google.cloud import pubsub_v1
 from google.cloud import storage
 import json
+from phdi_cloud_function_utils import _fail
+import flask
+from cloudevents.http import CloudEvent
+
+
+def log_error_and_generate_response(response: str, status: str) -> flask.Response:
+    """
+    Given an error message and and HTTP status code log the error and create a
+    flask.Response object containing the satus code and message.
+
+    :param response: An error message to be logged and included in the
+        returned flask.Response object.
+    :param status: An HTTP status code to be included in the returned flask.Response
+        object.
+    """
+    logging.error(response)
+    response = flask.Response(response=response, status=status)
+    return response
+
+
+def log_info_and_generate_response(response: str, status: str) -> flask.Response:
+    """
+    Given an info level message and and HTTP status code log the message and create a
+    flask.Response object containing the satus code and message.
+
+    :param response: An info level message to be logged and included in the
+        returned flask.Response object.
+    :param status: An HTTP status code to be included in the returned flask.Response
+        object.
+    """
+    logging.info(response)
+    response = flask.Response(response=response, status=status)
+    return response
 
 
 @functions_framework.cloud_event
-def read_source_data(cloud_event):
+def read_source_data(cloud_event: CloudEvent) -> flask.Response:
+    """
+    When this function is triggered with a CloudEvent payload read the new file if its
+    name begins with 'source-data/', identify each individual messsage
+    (ELR, VXU, or eCR) contained in the file, and publish each one to a GCP pubsub
+    topic. PROJECT_ID and INGESTION_TOPIC must be set as environment variables
+    specifying the pubsub topic to publish to and the GCP project it is located in.
+
+    :param cloud_event: A CloudEvent object provided by GCP whenever a new file is
+        written to the storage bucket containing source data to be ingested.
+    """
 
     # Extract buck and file names.
     try:
         filename = cloud_event.data["name"]
         bucket_name = cloud_event.data["bucket"]
-    except AttributeError:
-        logging.error("Bad CloudEvent payload - 'data' attribute missing.")
-    except KeyError:
-        logging.error(
-            "Bad CloudEvent payload - 'name' or 'bucket' name was not included."
-        )
-        return
 
-    # Load environment variables.
-    try:
-        project_id = os.environ["PROJECT_ID"]
-        ingestion_topic = os.environ["INGESTION_TOPIC"]
+    except AttributeError:
+        message = "Bad CloudEvent payload - 'data' attribute missing."
+        response = log_error_and_generate_response(message=message, status="400")
+        return response
+
     except KeyError:
-        logging.error(
-            "Missing required environment variables. Values for PROJECT_ID and TOPIC_ID must be set."
-        )
-        return
+        message = "Bad CloudEvent payload - 'name' or 'bucket' name was not included."
+        response = log_error_and_generate_response(message=message, status="400")
+        return response
 
     # Determine data type and root template.
     filename_parts = filename.split("/")
     if filename_parts[0] == "source-data":
+
         if filename_parts[1] == "elr":
             message_type = "hl7v2"
             root_template = "ORU_R01"
@@ -48,8 +85,25 @@ def read_source_data(cloud_event):
             root_template = "CCD"
 
         else:
-            logging.error("Unknown message type. Messages should be ELR, VXU, or eCR.")
-            return
+            message = "Unknown message type. Messages should be ELR, VXU, or eCR."
+            response = log_error_and_generate_response(message=message, status="400")
+            return response
+    else:
+        response = (
+            f"{filename} was not read because it does not begin with 'source-data/.'"
+        )
+        response = log_info_and_generate_response(response=response, status="200")
+        return response
+
+    # Load environment variables.
+    try:
+        project_id = os.environ["PROJECT_ID"]
+        ingestion_topic = os.environ["INGESTION_TOPIC"]
+
+    except KeyError:
+        message = "Missing required environment variables. Values for PROJECT_ID and TOPIC_ID must be set."
+        response = log_error_and_generate_response(message=message, status="400")
+        return response
 
     # Read file.
     storage_client = storage.Client()
@@ -60,17 +114,20 @@ def read_source_data(cloud_event):
     # Handle batch Hl7v2 messages.
     if message_type == "hl7v2":
         messages = convert_batch_messages_to_list(file_contents)
+
     else:
         messages = [file_contents]
 
     # Publish messages to pub/sub topic
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, ingestion_topic)
+    failure_count = 0
     for idx, message in enumerate(messages):
         pubsub_message = {
             "message": message,
             "message_type": message_type,
             "root_template": root_template,
+            "filename": filename,
         }
         pubsub_message = json.dumps(pubsub_message).encode("utf-8")
         future = publisher.publish(
@@ -104,7 +161,7 @@ def read_source_data(cloud_event):
                 failure_filename = filename.split("/")
                 failure_filename[0] = "publishing-failures"
                 failure_filename[-1] = (
-                    failure_filename[-1].split(".")[0:-1] + "-{idx}.txt"
+                    ".".join(failure_filename[-1].split(".")[0:-1]) + "-{idx}.txt"
                 )
                 failure_filename = "/".join(failure_filename)
                 blob = bucket.blob(failure_filename)
@@ -112,4 +169,8 @@ def read_source_data(cloud_event):
                 logging.info(
                     "Message {idx} in {filename} was written to {failure_filename} in {bucket_name}."
                 )
-        logging.info(f"Processed {filename}. Any messages that could not be published to the specified pubsub topic were written to {bucket}/publishing-failures/.")
+                failure_count += 1
+
+    response = f"Processed {filename}, which contained {idx+1} messages, of which {idx+1-failure_count} were successfully published, and {failure_count} could not be published."
+    response = log_info_and_generate_response(response=response, status="200")
+    return response
