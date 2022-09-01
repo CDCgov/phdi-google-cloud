e5c2a702fd1d7775af3b0c598aac381de413244d
DanSkylight <dan@skylight.digital>
2022-09-01 14:32:34 -0400

    Fix read_source_data Terraform.

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -68,6 +68,14 @@ data "archive_file" "read_source_data" {
 # Add source code zip to the Cloud Function's bucket
 resource "google_storage_bucket_object" "read_source_data_source_zip" {
   source       = data.archive_file.read_source_data.output_path
+  content_type = "application/zip"
+
+  # Append to the MD5 checksum of the files's content
+  # to force the zip to be updated as soon as a change occurs
+  name   = "src-${terraform.workspace}-${data.archive_file.read_source_data.output_md5}.zip"
+  bucket = google_storage_bucket.functions.name
+}
+
 data "archive_file" "add_patient_hash" {
   type        = "zip"
   source_dir  = "../../cloud-functions/add_patient_hash"
