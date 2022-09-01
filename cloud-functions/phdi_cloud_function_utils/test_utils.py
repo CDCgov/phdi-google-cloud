b0450a7f26b1a9c302c94ee506a2bedf29415303
DanSkylight <dan@skylight.digital>
2022-09-01 00:06:57 -0400

    Update the phdi_cloud_function_utils module.

--- cloud-functions/phdi_cloud_function_utils/test_utils.py
+++ cloud-functions/phdi_cloud_function_utils/test_utils.py
@@ -1,25 +1,59 @@
 import copy
 import json
 from phdi_cloud_function_utils import (
+    make_response,
     validate_fhir_bundle_or_resource,
     validate_request_body_json,
     validate_request_header,
-    fail,
-    success,
+    log_error_and_generate_response,
+    log_info_and_generate_response,
 )
 from unittest import mock
-
+import pytest
+import flask
 
 test_request_body = json.load(open("../assets/single_patient_bundle.json", "r"))
 
 
+def test_make_response():
+    status_code = 200
+    message = "some message"
+    json_payload = {"some-key": "some-value"}
+
+    with pytest.raises(ValueError):
+        make_response(
+            status_code=status_code, message=message, json_payload=json_payload
+        )
+    with pytest.raises(ValueError):
+        make_response(status_code=status_code)
+
+    expected_response = flask.Response()
+    expected_response.response = message
+    expected_response.status_code = status_code
+
+    response = make_response(status_code=status_code, message=message)
+    assert response.response == expected_response.response
+    assert response.status_code == expected_response.status_code
+
+    expected_response = flask.Response()
+    expected_response.response = json.dumps(json_payload)
+    expected_response.status_code = status_code
+    expected_response.mimetype = "application/json"
+    expected_response.headers = {"Content-Type": "application/json"}
+
+    response = make_response(status_code=status_code, json_payload=json_payload)
+    assert response.response == expected_response.response
+    assert response.status_code == expected_response.status_code
+    assert response.mimetype == expected_response.mimetype
+    assert response.headers == expected_response.headers
+
+
 def test_utils_bad_header():
     request = mock.Mock(headers={"Content-Type": "not-application/json"})
-    expected_result = fail(
-        status="Bad Request",
+    expected_result = make_response(
+        status_code=400,
         message="Header must include: 'Content-Type:application/json'.",
     )
-    expected_result.status_code = 400
 
     result = validate_request_header(request, "application/json")
     assert result.status == expected_result.status
@@ -29,7 +63,7 @@ def test_utils_bad_header():
 
 def test_utils_good_header():
     request = mock.Mock(headers={"Content-Type": "application/json"})
-    expected_result = success()
+    expected_result = make_response(status_code=200, message="Validation Succeeded!")
 
     result = validate_request_header(request, "application/json")
     assert result.status == expected_result.status
@@ -40,7 +74,7 @@ def test_utils_good_header():
 def test_utils_good_body():
     request = mock.Mock(headers={"Content-Type": "application/json"})
     request.get_json.return_value = test_request_body
-    expected_result = success()
+    expected_result = make_response(status_code=200, message="Validation Succeeded!")
     result = validate_request_body_json(request)
     assert result.status == expected_result.status
     assert result.status_code == expected_result.status_code
@@ -50,8 +84,8 @@ def test_utils_good_body():
 def test_utils_bad_body():
     request = mock.Mock(headers={"Content-Type": "application/json"})
     request.is_json.return_value = False
-    expected_result = fail(
-        message="Invalid request body - Invalid JSON", status="Bad Request"
+    expected_result = make_response(
+        status_code=400, message="Invalid request body - Invalid JSON"
     )
     result = validate_request_body_json(request)
     assert result.status == expected_result.status
@@ -69,8 +103,7 @@ def test_utils_bad_resource_type():
         + "The request body must contain a valid FHIR bundle or resource."
     )
     mock_request.get_json.return_value = body_with_wrong_resource_type
-    expected_result = fail(message=error_message, status="Bad Request")
-    expected_result.status_code = 400
+    expected_result = make_response(status_code=400, message=error_message)
     result = validate_fhir_bundle_or_resource(request=mock_request)
     assert result.status == expected_result.status
     assert result.status_code == expected_result.status_code
@@ -80,7 +113,7 @@ def test_utils_bad_resource_type():
 def test_utils_request():
     request = mock.Mock(headers={"Content-Type": "application/json"})
 
-    expected_result = success()
+    expected_result = make_response(status_code=200, message="Validation Succeeded!")
     request.get_json.return_value = test_request_body
     result = validate_fhir_bundle_or_resource(request)
 
@@ -89,8 +122,21 @@ def test_utils_request():
     assert result.response == expected_result.response
 
 
-def test_success_with_json():
-    result = success(json_payload=test_request_body)
-    expected_result = test_request_body
-    print(result.get_json())
-    assert result.get_json() == expected_result
+def test_log_info_and_generate_response():
+    response = log_info_and_generate_response(200, "my-response")
+    expected_response = flask.Response()
+    expected_response.response = "my-response"
+    expected_response.status_code = 200
+
+    assert response.response == expected_response.response
+    assert response.status_code == expected_response.status_code
+
+
+def test_log_error_and_generate_response():
+    response = log_error_and_generate_response(400, "my-response")
+    expected_response = flask.Response()
+    expected_response.response = "my-response"
+    expected_response.status_code = 400
+
+    assert response.response == expected_response.response
+    assert response.status_code == expected_response.status_code
